64156362f7fa5ac70d4b0196413f41b076d176a9
Jerry <azhou11111@163.com>
2014-07-08 14:12:40 +0800

    improve functions

--- trunk/jni/smart_card_jni_interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef SMART_CARD_JNI_INTERFACE_H_
-#define SMART_CARD_JNI_INTERFACE_H_
-
-const char* smart_card_get_class_name();
-JNINativeMethod* smart_card_get_methods(int* pCount);
-
-#endif /* SMART_CARD_JNI_INTERFACE_H_ */
